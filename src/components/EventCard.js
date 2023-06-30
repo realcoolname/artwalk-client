639e04b056f26cad7376965f8d2722748b2864b2
“Gokce <gokceberndt@gmail.com>
2023-06-30 16:25:29 +0200

    "events and venues list pages done"

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,5 +1,30 @@
-function EventCard () {
-    return <h1>This is the Event Card</h1>
+import React from "react";
+
+
+function EventCard(props) {
+  const { event } = props;
+
+
+
+  // Convert the date string to a JavaScript Date object
+  const eventDate = new Date(event.date);
+
+  // Format the date with specific options
+  const options = { day: "numeric", month: "short", year: "numeric" };
+  const formattedDate = eventDate.toLocaleDateString(undefined, options);
+
+  return (
+    <div className='event-card'>
+      <h1>IMAGE OF THE EVENT</h1>
+      <h2>{event.name}</h2>
+      <p>Curated by: {event.curator}</p>
+      <p>Venue: {event.venue}</p>
+      <p>{formattedDate}</p>
+      <p>{event.discipline}</p>
+      <p>{event.description}</p>
+      
+    </div>
+  );
 }
 
 export default EventCard;
--- src/components/VenueCard.js
+++ src/components/VenueCard.js
@@ -1,5 +1,21 @@
-function VenueCard () {
-    return <h1>This is the Venue Card</h1>
+import React from "react";
+
+
+function VenueCard(props) {
+  const { venue } = props;
+
+  return (
+    <div className='event-card'>
+      <h1>IMAGE OF THE VENUE</h1>
+      <h2>{venue.name}</h2>
+      <p>{venue.address.street} {venue.address.number}, {venue.address.zip}</p>
+      <p>{venue.address.city}, {venue.address.country}</p>
+      <p>{venue.description}</p>
+      <p>{venue.website}</p>
+      
+    </div>
+  );
 }
+
 
 export default VenueCard;
