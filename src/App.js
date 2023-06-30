644c87a7300458113ead37378ef614ed271a1b3a
“Gokce <gokceberndt@gmail.com>
2023-06-30 13:00:57 +0200

    "navbar done"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav } from "react-bootstrap";
 import "./App.css";
 
-import { Route, Routes, NavLink, Link } from 'react-router-dom'
+import { Route, Routes, NavLink, Link } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import EventsListPage from "./pages/EventsListPage";
 import VenuesListPage from "./pages/VenuesListPage";
 
+import NavBar from "./components/NavBar";
 import VenueCard from "./components/VenueCard";
-import Navbar from "./components/Navbar";
 import EventCard from "./components/EventCard";
 import Carousel from "./components/Caroussel";
-import LogIn from './components/LogIn';
-import SignUp from './components/SignUp';
-import UpdateEventForm from "./components/UpdateEventForm"
-
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+import UpdateEventForm from "./components/UpdateEventForm";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NavBar />
+      
 
       <Routes>
         <Route exact path="/" element={<Homepage />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,37 @@
-function Navbar () {
-    return <h1>This is the Navbar</h1>
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+      <Navbar className="navbar" expand="lg">
+        <Navbar.Brand as={NavLink} to="/">
+          ARTWALK
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/events">
+              Events
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/venues">
+              Venues
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={NavLink} to="/login">
+              Log In
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/signup">
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+  );
 }
 
-export default Navbar;
+export default NavBar;
