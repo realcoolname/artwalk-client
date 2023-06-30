692195601acabec08bb0a3781976e1a27f8d323e
“Gokce <gokceberndt@gmail.com>
2023-06-30 10:39:33 +0200

    bootstrap set up

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,12 @@
+import { Button } from 'react-bootstrap';
+
 function Homepage () {
-    return <h1>This is the Homepage</h1>
+    return (
+    <div>
+        <h1>This is the Homepage</h1>
+        <Button className='btn-color'>Click me!</Button>
+    </div>
+   )
 }
 
 export default Homepage;
